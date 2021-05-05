4f5643b3578f0ed5b09d5d7b448dcc688fb9ca59
ripytide <james.forsterer@gmail.com>
2021-05-05 20:34:12 +0100

    increase notification time and use fish and update backup script

--- .bashrc
+++ .bashrc
@@ -26,3 +26,5 @@ export FZF_DEFAULT_OPTS='
   --preview="bat --color=always {}"
 '
 colorscript random
+
+exec fish
--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,92 +1,74 @@
 #!/bin/bash
-LOGFILE="/home/ripytide/logs/Borg.txt"
 
 # Close if borg running
 if pgrep "borg" > /dev/null 
 then
-    echo "$(date "+%m-%d-%Y %T") : Backup already running, exiting" 2>&1 | tee -a $LOGFILE | notify-pipe.sh critical
-    exit
+    notify-send "Backup already running, exiting!" -u critcal
     exit
 fi
 
-# Close if parity sync started (disabled for now)
-#PARITYCHK=$(/root/mdcmd status | egrep STARTED)
-#if [[ $PARITYCHK == *"STARTED"* ]]; then
-#    echo "Parity check running, exiting"
-#    exit
-#    exit
-#fi
 
 
 #This is the location your Borg program will store the backup data to
-export BORG_REPO='/home/ripytide/backup'
+export BORG_REPO="/home/ripytide/backup"
 
 #This is the location you want Rclone to send the BORG_REPO to 
-export CLOUDDEST='gdrive:backup'
-
-#Speeds up the backup by caching more
-#export BORG_FILES_CACHE_TTL=26
+export CLOUDDEST="gdrive:${HOSTNAME}-backup"
 
 #Setting this, so you won't be asked for your repository passphrase:
-export BORG_PASSPHRASE='1RizzleislandironitE9'
-
-#Backup my unraid directories into an archive
-SECONDS=0
-
-echo "$(date "+%m-%d-%Y %T") : Borg backup has started" 2>&1 | tee -a $LOGFILE | notify-pipe.sh normal
-borg create                         \
-    --verbose                       \
-    --info                          \
-    --list                          \
-    --filter AMEx                   \
-    --files-cache=mtime,size        \
-    --stats                         \
-    --show-rc                       \
-    --compression lz4               \
-    --exclude-caches                \
-    --exclude-from /home/ripytide/backup_exclude   \
-    \
-	$BORG_REPO::'${HOSTNAME}-$(borg list /home/backup --short | wc -l)'  \
-    /home/ripytide					\
-
-    >> $LOGFILE 2>&1
+export BORG_PASSPHRASE="1RizzleislandironitE9"
 
 
+
+#Backup name
+backup_name="${HOSTNAME}-$(date +%S-%M-%H-%d-%m-%Y)"
+
+#Backup
+borg create $BORG_REPO::"$backup_name" --patterns-from ~/backup_patterns --exclude-from ~/backup_exclude -p --stats --compression lz4 --exclude-caches --list
 backup_exit=$?
-# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+
+if [ $backup_exit -eq 0 ]
+then
+	notify-send "Backup $backup_name created successfully!"
+else
+	notify-send "Backup Create failed, exiting!" -u critical
+	exit
+fi
+
 # archives of THIS machine. The '{hostname}-' prefix is very important to
 # limit prune's operation to this machine's archives and not apply to
 # other machines' archives also:
-#echo "$(date "+%m-%d-%Y %T") : Borg pruning has started" 2>&1 | tee -a $LOGFILE
-borg prune                          \
-    --list                          \
-    --prefix '{hostname}-'          \
-    --show-rc                       \
-    --keep-daily    7               \
-    --keep-weekly   4               \
-    --keep-monthly  6               \
-    >> $LOGFILE 2>&1
 
+#Prune
+borg prune --list --prefix '{hostname}-' --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-last 6 >> ~/logs/prunelog.txt
 prune_exit=$?
-#echo "$(date "+%m-%d-%Y %T") : Borg pruning has completed" 2>&1 | tee -a $LOGFILE
 
-# use highest exit code as global exit code
-global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
-
-# Execute if no errors
-if [ ${global_exit} -eq 0 ];
+if [ $prune_exit -eq 0 ]
 then
-    borgstart=$SECONDS
-    echo "$(date "+%m-%d-%Y %T") : Borg backup completed in  $(($borgstart/ 3600))h:$(($borgstart% 3600/60))m:$(($borgstart% 60))s" | tee -a >> $LOGFILE 2>&1 | notify-pipe.sh normal
-
-#Reset timer
-    SECONDS=0
-    echo "$(date "+%m-%d-%Y %T") : Rclone Borg sync has started" >> $LOGFILE
-    rclone sync $BORG_REPO $CLOUDDEST -P --stats 1s -v 2>&1 | tee -a $LOGFILE
-    rclonestart=$SECONDS   
-    echo "$(date "+%m-%d-%Y %T") : Rclone Borg sync completed in  $(($rclonestart/ 3600))h:$(($rclonestart% 3600/60))m:$(($rclonestart% 60))s" 2>&1 | tee -a $LOGFILE | notify-pipe.sh normal
-# All other errors
+	notify-send "Prune successful!"
 else
-    echo "$(date "+%m-%d-%Y %T") : Borg has errors code:" $global_exit 2>&1 | tee -a $LOGFILE | notify-pipe.sh critical
+	notify-send "Prune failed, exiting!" -u critical
+	exit
 fi
-exit ${global_exit}
+
+#Get repo size
+repo_size=$(borg info $BORG_REPO --json | jq .cache.stats.unique_csize)
+repo_size="$(expr $repo_size / 1000000)MB"
+
+upload_confirm=$(echo -e "Yes\nNo" | rofi -dmenu -i -p "Sync $repo_size to cloud?")
+
+if [ $upload_confirm = "Yes" ]
+then
+	rclone sync $BORG_REPO $CLOUDDEST -P
+	rclone_exit=$?
+
+	if [ $rclone_exit -eq 0 ]
+	then
+		notify-send "RClone sync successful!"
+	else
+		notify-send "RClone sync failed, exiting!" -u critical
+		exit
+	fi
+else
+	notify-send "Manual upload cancelled!" "Please address the issue and then manually backup again" -u critical
+fi
