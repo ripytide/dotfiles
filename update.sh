caa00758b3ebb94d4254a42a41f7265d75cc1963
ripytide <james.forsterer@gmail.com>
2021-09-25 12:45:50 +0100

    remove compile from update as requires on system

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-~/dotfiles/compile.sh
 sudo pacman -Syu --color
 curl https://raw.githubusercontent.com/ripytide/dotfiles/main/packages.txt -o ~/dotfiles/packages.txt
 yay -S --noconfirm --needed - < ~/dotfiles/packages.txt
