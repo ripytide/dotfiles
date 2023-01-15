f188ffd8da82778209599456781bff097ec72206
ripytide <james.forsterer@gmail.com>
2023-01-15 20:35:38 +0000

    add new compile script with envsubst

---
+++ compile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+subprocess.run(["cp", "-r", "/home/ripytide/dotfiles/actual/.", "/home/ripytide"])
+
+all_dotfiles = []
+# traverse root directory, and list directories as dirs and files as files
+for root, dirs, files in os.walk("/home/ripytide/dotfiles/actual"):
+    # print(os.path.basename(root))
+    for file in files:
+        all_dotfiles.append(root + "/" + file)
+
+
+def new_location(old):
+    return old.replace("/home/ripytide/dotfiles/actual/", "/home/ripytide/")
+
+
+for dotfile in all_dotfiles:
+    command = f"cat {dotfile} | envsubst | tee {new_location(dotfile)}"
+    print(command)
+    subprocess.getoutput(command)
