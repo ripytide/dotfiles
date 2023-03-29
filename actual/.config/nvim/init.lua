10daf25469e21042ead2709f71968e0018bf7342
ripytide <james.forsterer@gmail.com>
2023-03-29 10:34:06 +0100

    reload treesitter on every save to fix it breaking sometimes

--- actual/.config/nvim/init.lua
+++ actual/.config/nvim/init.lua
@@ -22,3 +22,16 @@ vim.cmd.colorscheme("tokyonight-night")
 vim.cmd("hi Search guibg=black guifg=red")
 vim.cmd("hi CurSearch guibg=black guifg=blue")
 vim.cmd("hi IncSearch guibg=green guifg=black")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd(
+	'BufWrite',
+	{
+		pattern = '*.scm',
+		callback = function ()
+			require'nvim-treesitter.query'.invalidate_query_cache()
+		end
+	}
+)
