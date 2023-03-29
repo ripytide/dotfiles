6b59032f1a269d0070b7901441842cb57fa5ea61
ripytide <james.forsterer@gmail.com>
2023-03-29 11:57:26 +0100

    merged by zoxide and other telescope configs together

--- actual/.config/nvim/lua/telescope_config.lua
+++ actual/.config/nvim/lua/telescope_config.lua
@@ -1,6 +1,7 @@
 local telescope = require("telescope")
 local telescopeConfig = require("telescope.config")
 local actions = require("telescope.actions")
+local z_utils = require("telescope._extensions.zoxide.utils")
 
 -- Clone the default Telescope configuration
 local vimgrep_arguments = {unpack(telescopeConfig.values.vimgrep_arguments)}
@@ -23,8 +24,31 @@ telescope.setup({
             -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
             find_command = {"rg", "--files", "--hidden", "--glob", "!.git/*"}
         }
-    }
+    },
+	extensions = {
+        zoxide = {
+            prompt_title = "[ Zoxide ]",
+            mappings = {
+                default = {
+                    after_action = function(selection)
+                        print("Update to (" .. selection.z_score .. ") " ..
+                                  selection.path)
+                    end
+                },
+                ["<C-s>"] = {
+                    before_action = function(_)
+                        print("before C-s")
+                    end,
+                    action = function(selection)
+                        vim.cmd("edit " .. selection.path)
+                    end
+                },
+                ["<C-q>"] = {action = z_utils.create_basic_command("split")}
+            }
+        }
+	}
 })
 
 telescope.load_extension("frecency")
 telescope.load_extension("ui-select")
+telescope.load_extension("zoxide")
