514320c5e9ce49b09a955b30f88c7a6c16709199
ripytide <james.forster@toshiba-bril.com>
2023-06-07 17:54:03 +0100

    fixes for ubuntu

--- actual/.config/fish/functions/ls.fish
+++ actual/.config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
 function ls --wraps='exa -l --icons' --description 'alias ls exa -l --icons'
-  exa --long --icons --group-directories-first --git $argv
+  exa --long --icons --group-directories-first $argv
         
 end
--- compile.py
+++ compile.py
@@ -4,26 +4,26 @@ import os
 import subprocess
 import socket
 
-subprocess.run(["cp", "-a", "/home/ripytide/dotfiles/actual/.", "/home/ripytide"])
+subprocess.run(["cp", "-a", "/home/jamesforester/dotfiles/actual/.", "/home/jamesforester"])
 
 all_dotfiles = []
 # traverse root directory, and list directories as dirs and files as files
-for root, dirs, files in os.walk("/home/ripytide/dotfiles/actual"):
+for root, dirs, files in os.walk("/home/jamesforester/dotfiles/actual"):
     # print(os.path.basename(root))
     for file in files:
         all_dotfiles.append(root + "/" + file)
 
 
 def new_location(old):
-    return old.replace("/home/ripytide/dotfiles/actual/", "/home/ripytide/")
+    return old.replace("/home/jamesforester/dotfiles/actual/", "/home/jamesforester/")
 
 
 hostname = socket.gethostname()
-
-variables = {"muncher": {"$MONITOR": "eDP-1", "$UNDODIR": "~/sync/undodir", "$KEYBOARD": "AT Translated Set 2 keyboard"}, "nipper": {"$MONITOR": "HDMI-2", "$UNDODIR": "~/undodir", "$KEYBOARD": "Dell KB216 Wired Keyboard"}}
+variables = {"muncher": {"$MONITOR": "eDP-1", "$UNDODIR": "~/sync/undodir", "$KEYBOARD": "AT Translated Set 2 keyboard"}, "nipper": {"$MONITOR": "HDMI-2", "$UNDODIR": "~/undodir", "$KEYBOARD": "Dell KB216 Wired Keyboard"}, "C273": {"$KEYBOARD": "Lenovo Lenovo Traditional USB Keyboard", "$UNDODIR": "~/undodir"}}
 
 for dotfile in all_dotfiles:
     with open(dotfile) as input:
+        print(f"Opening {dotfile}")
         string = input.read()
         for (key, value) in variables[hostname].items():
             string = string.replace(key, value)
