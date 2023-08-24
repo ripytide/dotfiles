3f58bdc5a76c037e2c0573c1bd38afd69c9f6f43
ripytide <james.forsterer@gmail.com>
2023-08-24 18:01:26 +0100

    added hardware acceleration for nvidia

--- actual/.config/nixos/configuration.nix
+++ actual/.config/nixos/configuration.nix
@@ -1,23 +1,15 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running `nixos-help`).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+
   system.stateVersion = "22.05"; # Did you read the comment?
-  
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 
@@ -47,7 +39,7 @@
   networking.hostName = "devourer"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -75,7 +67,7 @@
   users.users.ripytide = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
-    packages = with pkgs; [];
+    packages = with pkgs; [ ];
   };
 
   security.sudo = {
@@ -91,10 +83,19 @@
     }];
   };
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
   };
 
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -110,67 +111,64 @@
     enableNvidiaPatches = true;
   };
 
-  fonts.packages = with pkgs; [
-	nerdfonts
-	noto-fonts
-	noto-fonts-emoji
-    fira-code
-    fira-code-nerdfont
-	fira-code-symbols
-  ];
-
-  environment.systemPackages = with pkgs; [
-  	# Desktop Stuff
-	fish
-    dunst
-	waybar
-	ydotool
-	starship
-	playerctl
-	pavucontrol
-	rofi-wayland
-	pokemon-colorscripts-mac
-
-	# Core and Language Stuff
-	gcc
-	git
-	rustup
-	busybox
-	python312
-
-	# CLI Stuff
-	exa
-	fzf
-	bat
-	sxiv
-	delta
-	bottom
-	neovim
-	zoxide
-	lazygit
-	ripgrep
-	tealdeer
-	rm-improved
-	pass-wayland
-
-	# GUI Stuff
-	kitty
-	firefox
-	libreoffice-fresh
-	kicad
-	freecad
-
-	# Custom Stuff
-	(import ./packages/evremap.nix {inherit pkgs;})
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-	pinentryFlavor = "gnome3";
+    pinentryFlavor = "gnome3";
   };
-}
 
+  fonts.packages = with pkgs; [
+    nerdfonts
+    noto-fonts
+    noto-fonts-emoji
+    fira-code
+    fira-code-nerdfont
+    fira-code-symbols
+  ];
+
+  environment.systemPackages = with pkgs; [
+    # Desktop Stuff
+    fish
+    dunst
+    waybar
+    ydotool
+    starship
+    playerctl
+    pavucontrol
+    wl-clipboard
+    rofi-wayland
+    pokemon-colorscripts-mac
+
+    # Core and Language Stuff
+    gcc
+    git
+    rustup
+    busybox
+    python312
+
+    # CLI Stuff
+    exa
+    fzf
+    bat
+    sxiv
+    delta
+    bottom
+    neovim
+    zoxide
+    lazygit
+    ripgrep
+    tealdeer
+    rm-improved
+    pass-wayland
+
+    # GUI Stuff
+    kitty
+    firefox
+    libreoffice-fresh
+    kicad
+    freecad
+
+    # Custom Stuff
+    (import ./packages/evremap.nix { inherit pkgs; })
+  ];
+}
