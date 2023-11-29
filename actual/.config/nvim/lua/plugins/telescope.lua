9dc8865886438841d6bc5e9675213a2da888a2ad
ripytide <james.forsterer@gmail.com>
2023-11-29 23:32:16 +0000

    made telescope grep and find hidden files

--- actual/.config/nvim/lua/plugins/telescope.lua
+++ actual/.config/nvim/lua/plugins/telescope.lua
@@ -12,9 +12,28 @@ return {
           ["<C-q>"] = require("telescope.actions").send_to_qflist,
         },
       },
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+		"--hidden"
+      },
+      file_ignore_patterns = {
+        "node_modules",
+        ".git",
+      },
       layout_config = {
         width = 500,
         height = 500,
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
       },
     },
   },
