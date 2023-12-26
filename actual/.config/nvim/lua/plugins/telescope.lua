af6a2ecb85e05390a46ac200310fe956e7309736
ripytide <james.forsterer@gmail.com>
2023-12-26 10:35:22 +0000

    refactor the telescope config

--- actual/.config/nvim/lua/plugins/telescope.lua
+++ actual/.config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,26 @@
 return {
-	"nvim-telescope/telescope.nvim",
+	"telescope.nvim",
+	dependencies = {
+		{
+			"debugloop/telescope-undo.nvim",
+		},
+		{
+			"gbrlsnchs/telescope-lsp-handlers.nvim",
+		},
+	},
+	config = function(_, opts)
+		require("telescope").setup(opts)
+		require("telescope").load_extension("undo")
+		require("telescope").load_extension("lsp_handlers")
+	end,
+	keys = { { "<leader>cu", "<cmd>Telescope undo<CR>", desc = "Telescope Undo History" } },
 	opts = {
 		defaults = {
 			mappings = {
 				i = {
 					["<esc>"] = require("telescope.actions").close,
 					["<C-u>"] = false,
+					["<C-k>"] = false,
 					["<C-q>"] = require("telescope.actions").send_to_qflist,
 				},
 				n = {
