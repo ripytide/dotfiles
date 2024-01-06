0e9a953eb624f120be12861d3ad4abb8e57c3fea
ripytide <james.forsterer@gmail.com>
2024-01-06 17:35:14 +0000

    save current

--- actual/.config/nvim/lua/config/options.lua
+++ actual/.config/nvim/lua/config/options.lua
@@ -17,6 +17,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.mouse = ""
 vim.opt.scrolloff = 8
+vim.opt.textwidth = 80
 
 vim.cmd("hi Search guibg=black guifg=red")
 vim.cmd("hi CurSearch guibg=black guifg=blue")
