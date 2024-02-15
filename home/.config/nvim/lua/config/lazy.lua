d58195409ab96da78e239b98823b7cbe30b1620d
ripytide <james.forsterer@gmail.com>
2024-02-15 14:23:45 +0000

    update

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		-- import any extras modules here
 		{ import = "lazyvim.plugins.extras.lang.rust" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
