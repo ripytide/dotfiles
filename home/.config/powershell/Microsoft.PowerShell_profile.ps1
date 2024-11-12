376500f4073c9da8f91d720695b468947cf1ff68
James Forster <jf154126@renishaw.com>
2024-11-12 11:40:03 +0000

    revert powershell config

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,4 @@
-Function ls-function {eza --long --group --icons --group-directories-first --git -s modified $args}
-Function la-function {eza --all --long --group --icons --group-directories-first --git -s modified $args}
-Function tree-function {eza --tree --git-ignore --icons $args}
-Set-Alias -Name ls -Value ls-function -Option AllScope
-Set-Alias -Name la -Value la-function -Option AllScope
-Set-Alias -Name tree -Value tree-function -Option AllScope
-
-Invoke-Expression (&starship init powershell)
-Invoke-Expression (& { (zoxide init powershell --cmd n | Out-String) })
-
 Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -PredictionViewStyle List
-Set-PSReadLineOption -ShowToolTips:$false
-Set-PSReadLineKeyHandler -Chord Ctrl+j -Function NextSuggestion
-Set-PSReadLineKeyHandler -Chord Ctrl+k -Function PreviousSuggestion
-Remove-PSReadLineKeyHandler -Chord Ctrl+n
-Remove-PSReadLineKeyHandler -Chord Ctrl+p
 
 # Install-PSResource -Name PSReadLine
 # Import-Module -Name CompletionPredictor
