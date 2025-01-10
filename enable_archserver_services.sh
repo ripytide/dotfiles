7470bf59ffb459f02470ea46f2e261c2ce9b6c44
ripytide <james.forsterer@gmail.com>
2025-01-10 17:09:02 +0000

    add samba to dotfiles

--- enable_archserver_services.sh
+++ enable_archserver_services.sh
@@ -5,6 +5,9 @@ systemctl enable --now frpc
 systemctl enable --now caddy
 systemctl enable --now mosquitto
 systemctl enable --now zigbee2mqtt
+systemctl enable --now smb
+systemctl enable --now wsdd
 
 systemctl enable --now docker
+systemctl enable --now avahi-daemon
 systemctl enable --now sshd
