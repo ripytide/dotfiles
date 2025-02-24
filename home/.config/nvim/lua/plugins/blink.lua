bd3de1ca2217a4546f398df0f2beaab0215535d2
James Forster <jf154126@renishaw.com>
2025-02-24 08:42:50 +0000

    fix blink

--- home/.config/nvim/lua/plugins/blink.lua
+++ home/.config/nvim/lua/plugins/blink.lua
@@ -43,19 +43,19 @@ return {
 		cmdline = {
 			enabled = true,
 		},
-		sources = {
-			cmdline = function()
-				local type = vim.fn.getcmdtype()
-				-- Search forward and backward
-				if type == "/" or type == "?" then
-					return { "buffer" }
-				end
-				-- Commands
-				if type == ":" then
-					return { "cmdline" }
-				end
-				return {}
-			end,
-		},
+		-- sources = {
+		-- 	cmdline = function()
+		-- 		local type = vim.fn.getcmdtype()
+		-- 		-- Search forward and backward
+		-- 		if type == "/" or type == "?" then
+		-- 			return { "buffer" }
+		-- 		end
+		-- 		-- Commands
+		-- 		if type == ":" then
+		-- 			return { "cmdline" }
+		-- 		end
+		-- 		return {}
+		-- 	end,
+		-- },
 	},
 }
