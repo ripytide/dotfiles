2589d10a6349a263b3e8a77d6763fc9061dd264d
ripytide <james.forsterer@gmail.com>
2025-02-25 18:34:31 +0000

    update blink config and update lazyvim

--- home/.config/nvim/lua/plugins/blink.lua
+++ home/.config/nvim/lua/plugins/blink.lua
@@ -41,21 +41,32 @@ return {
 			},
 		},
 		cmdline = {
+			-- https://cmp.saghen.dev/configuration/reference#mode-specific
 			enabled = true,
+			sources = function()
+				local type = vim.fn.getcmdtype()
+				-- Search forward and backward
+				if type == "/" or type == "?" then
+					return { "buffer" }
+				end
+				-- Commands
+				if type == ":" or type == "@" then
+					return { "cmdline" }
+				end
+				return {}
+			end,
+			completion = {
+				trigger = {
+					show_on_blocked_trigger_characters = {},
+					show_on_x_blocked_trigger_characters = nil, -- Inherits from top level `completion.trigger.show_on_blocked_trigger_characters` config when not set
+				},
+				menu = {
+					auto_show = nil, -- Inherits from top level `completion.menu.auto_show` config when not set
+					draw = {
+						columns = { { "label", "label_description", gap = 1 } },
+					},
+				},
+			},
 		},
-		-- sources = {
-		-- 	cmdline = function()
-		-- 		local type = vim.fn.getcmdtype()
-		-- 		-- Search forward and backward
-		-- 		if type == "/" or type == "?" then
-		-- 			return { "buffer" }
-		-- 		end
-		-- 		-- Commands
-		-- 		if type == ":" then
-		-- 			return { "cmdline" }
-		-- 		end
-		-- 		return {}
-		-- 	end,
-		-- },
 	},
 }
