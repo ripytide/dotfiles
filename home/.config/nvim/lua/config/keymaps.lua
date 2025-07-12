23b0c080a6654b550fc7a77109ddc403c9e6ccf8
ripytide <james.forsterer@gmail.com>
2025-07-12 17:36:29 +0100

    update configs

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -5,16 +5,8 @@
 -- vim.keymap.set("n", "<C-d>", "16jzz")
 -- vim.keymap.set("n", "<C-u>", "16kzz")
 
-local diagnostic_goto = function(next, severity)
-	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-	severity = severity and vim.diagnostic.severity[severity] or nil
-	return function()
-		go({ severity = severity })
-	end
-end
-
-vim.keymap.set("n", "n", diagnostic_goto(true), { desc = "Jump to next diagnostic message" })
-vim.keymap.set("n", "N", diagnostic_goto(false), { desc = "Jump to previous diagnostic message" })
+vim.keymap.set("n", "n", function() vim.diagnostic.jump({count=1, float=true}) end, { desc = "Jump to next diagnostic message" })
+vim.keymap.set("n", "N", function() vim.diagnostic.jump({count=-1, float=true}) end, { desc = "Jump to previous diagnostic message" })
 vim.keymap.set("n", "_", "N", { desc = "Jump to previous search result" })
 vim.keymap.set("n", ",", "n", { desc = "Jump to next search result" })
 
@@ -23,27 +15,15 @@ vim.keymap.set("n", "P", "P`[", { desc = "Paste without moving the cursor" })
 
 vim.keymap.set("x", "p", "P", { desc = "Paste without overwriting the buffer" })
 
--- vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>jk", { desc = "Escape and clear hlsearch" })
-
 local substitute_config = { register = "" }
-vim.keymap.set("n", "s", function()
-	require("substitute").operator(substitute_config)
-end, { noremap = true })
-vim.keymap.set("n", "ss", function()
-	require("substitute").line(substitute_config)
-end, { noremap = true })
-vim.keymap.set("n", "S", function()
-	require("substitute").eol(substitute_config)
-end, { noremap = true })
-vim.keymap.set("x", "s", function()
-	require("substitute").visual(substitute_config)
-end, { noremap = true })
+vim.keymap.set("n", "s", function() require("substitute").operator(substitute_config) end, { noremap = true })
+vim.keymap.set("n", "ss", function() require("substitute").line(substitute_config) end, { noremap = true })
+vim.keymap.set("n", "S", function() require("substitute").eol(substitute_config) end, { noremap = true })
+vim.keymap.set("x", "s", function() require("substitute").visual(substitute_config) end, { noremap = true })
 
 vim.keymap.del("n", "<leader>gG")
 vim.keymap.set("n", "<leader>gg", require("lazy").show, { desc = "Open lazy.nvim" })
-vim.keymap.set("n", "<leader>l", function()
-	require("snacks").lazygit.open()
-end, { desc = "LazyGit" })
+vim.keymap.set("n", "<leader>l", function() require("snacks").lazygit.open() end, { desc = "LazyGit" })
 
 vim.keymap.set("n", ";n", "A,<esc>")
 vim.keymap.set("n", ";t", "A;<esc>")
--- home/.config/nvim/lua/plugins/blink.lua
+++ home/.config/nvim/lua/plugins/blink.lua
@@ -10,24 +10,20 @@ return {
 	---@type blink.cmp.Config
 	opts = {
 		keymap = {
-			preset = "enter",
-			["<Tab>"] = {
-				"snippet_forward",
-				"select_next",
-				function(cmp)
-					if has_words_before() then
-						return cmp.accept()
-					else
-						return false
-					end
-				end,
-				"fallback",
-			},
-			["<S-Tab>"] = {
-				"snippet_backward",
-				"select_prev",
-				"fallback",
-			},
+			preset = "none",
+
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide" },
+			["<C-y>"] = { "select_and_accept" },
+
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
 		},
 		completion = {
 			list = {
